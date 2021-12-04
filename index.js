ff063895406c79994230acab1717e4a17a6c061d
Steven <steven.jankowski1@gmail.com>
2021-12-04 02:52:19 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names){
+    let message = [];
+    for(let i = 0; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return message;
+}
+
+function countDown(number){
+    while (number >= 0){
+    console.log(number--)
+    }
+}
